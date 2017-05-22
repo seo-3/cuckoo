5fd9cea5a9fe708186fad3bb58bed11dd6d27569
akimitsu seo <akimitsu.seo@dragon.jp>
2017-05-22 13:13:41 +0900

    remove debug logs

--- cmd/cp.go
+++ cmd/cp.go
@@ -1,24 +1,9 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
 	"fmt"
 	"time"
 	"os"
-	// "log"
-	// "strings"
 	"github.com/Songmu/retry"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
@@ -26,7 +11,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/spf13/cobra"
 )
-
 
 // cpCmd represents the cp command
 var cpCmd = &cobra.Command{
@@ -39,7 +23,6 @@ Cobra is a svc library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("cp called")
 
 		err := execute()
 		if err != nil {
